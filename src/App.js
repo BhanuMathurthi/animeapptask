26633ad5d660f37bfc9fb78fa1a94b58c95c72d4
Bhanu Mathurthi <bhanumathurthi01@gmail.com>
2022-10-17 18:27:46 +0530

    made some changes in AnimeCard component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import DragAndDropAnimes from "./Components/DragAndDrop/DragAndDropAnime";
+import Home from "./Components/Home";
+import EachAnimeDetails from "./Components/IndividualAnimes/EachAnimeDetails";
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/anime/:id" element={<EachAnimeDetails />}></Route>
+        <Route path="/dragandropanimes" element={<DragAndDropAnimes />}></Route>
+      </Routes>
+    </div>
+  );
 }
+
+export default App;
